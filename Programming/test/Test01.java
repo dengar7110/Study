f4cd2d9d17ec9a6a1b90ba31ed33d51e10654fab
garden <124008155+dengar7110@users.noreply.github.com>
2024-05-17 21:33:45 +0900

    Create Test01.java
    0517

---
+++ Programming/test/Test01.java
@@ -0,0 +1,36 @@
+package test;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		
+//		1. 출력
+//		JAVA 이건 못참지
+		
+//		2. 여러 줄 출력
+//		println 을 세번 사용해서 아래 문장 형식으로 본인 소개를 세줄 출력 하세요.
+//		안녕하세요. 저는 김인규 입니다. 
+//		저는 호계동에 삽니다. 
+//		저는 누워있는 것을 좋아 합니다.
+		
+//		3. 여러 줄 출력
+//		println 을 한번 사용해서 아래 문장 형식으로 본인 소개를 세줄 출력 하세요.
+//		안녕하세요. 저는 김인규 입니다. 
+//		저는 30대 입니다. 
+//		놀랫죠?
+
+		//1번문제
+		System.out.println("JAVA 이건 못참지");
+		
+		
+		//2번문제
+		System.out.println("안녕하세요. 저는 김인규 입니다");
+		System.out.println("저는 호계동에 삽니다.");
+		System.out.println("저는 누워있는 것을 좋아 합니다.");
+		
+		
+		//3번문제
+		System.out.println("안녕하세요. 저는 김인규 입니다. \n저는 30대 입니다. \n놀랫죠?");
+	}
+
+}
