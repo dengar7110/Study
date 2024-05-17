0728429e4cc74e7eb252ce17a26f817db0cd6449
garden <124008155+dengar7110@users.noreply.github.com>
2024-05-17 19:22:22 +0900

    Add files via upload

---
+++ Ex01.java
@@ -0,0 +1,24 @@
+package ex;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hellow World!");
+		/*코드로 수행되기 위한 내용이 아닌
+		코드를 설명하기 위해 작성하는 내용
+		주석
+		
+		반갑습니다를 출력
+		*/
+		System.out.println("반갑습니다!");
+		
+		// 하나의 println 으로 여러줄 출력
+		// 개행문자 \n
+		System.out.println("저는 홍정원 입니다.\n잘 부탁드립니다.");
+		
+		System.out.print("안녕하세요");
+		System.out.println(" Java 수업입니다");
+	}
+
+}
