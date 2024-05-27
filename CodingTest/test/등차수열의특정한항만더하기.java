15f159c4bca51c54f346fa13bc28ebc789cd2539
garden <124008155+dengar7110@users.noreply.github.com>
2024-05-27 18:40:18 +0900

    Create 등차수열의특정한항만더하기.java
    0527

---
+++ CodingTest/test/등차수열의특정한항만더하기.java
@@ -0,0 +1,20 @@
+package CodingTest;
+
+public class 등차수열의특정한항만더하기 {
+	
+    public int solution(int a, int d, boolean[] included) {
+        
+    	
+    	int n = included.length;
+    	int[] equalSum = new int[n];
+    	
+    	int sum = 0;
+    	for(int i = 0; i < n; i++) {
+    		if(included[i]) {
+    			sum += equalSum[i] = a + i * d;
+    		}
+    	}
+    	return sum;
+    	
+    }
+}
