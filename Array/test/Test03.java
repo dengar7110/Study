25a0efa6a6f1665f10d5a04506dd0abebef7160a
garden <124008155+dengar7110@users.noreply.github.com>
2024-05-28 20:12:32 +0900

    Update Test03.java
    0528

--- Array/test/Test03.java
+++ Array/test/Test03.java
@@ -22,6 +22,7 @@ public class Test03 {
 		int[] numbers = {3, 5, 2, 10, 39};
 		
 		Scanner sc = new Scanner(System.in);
+		
 		System.out.print("두 개의 index 를 입력 하세요 : ");
 		int index1 = sc.nextInt();
 		int index2 = sc.nextInt();
@@ -30,9 +31,9 @@ public class Test03 {
 		numbers[index2] = tmp;
 		
 		for(int i = 0; i < numbers.length; i++) {
-			System.out.print(numbers[i] +" ");
+			System.out.print(numbers[i] + " ");
 		}
-		
+		System.out.println();
 		
 		
 //		2. 셔플
@@ -50,32 +51,26 @@ public class Test03 {
 //
 //		4 5 7 8 9 1 3 6 2 10
 		
-		System.out.println();
+		
 		System.out.println("1 ~ 10 까지 차례로 저장된 배열을 만들고, 순서를 뒤 섞어서 그 결과를 출력 하세요.");
 		
-		int[] test = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		int[] numbers2 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 	
 		Random rand = new Random();
-		
-		int temp; 
-		int temp2;
-		
-		////
+	
 		for (int i = 0; i < 100; i++) {
+			// 0 ~ 9
 			int randomNumber = rand.nextInt(10);
-			temp = test[randomNumber];
-			temp2 = test[randomNumber];
-			test[randomNumber] = temp2;
-			test[randomNumber] = temp;
-
+			tmp = numbers2[0];
+			numbers2[0] = numbers2[randomNumber];
+			numbers2[randomNumber] = tmp;
 		}
 		
-		for(int i = 0; i < test.length; i++) {
-			System.out.print(test[i] + " ");
+		for(int i = 0; i < numbers2.length; i++) {
+			System.out.print(numbers2[i] + " ");
 		}
-		
-		
 		System.out.println();
+		
 		
 //		3. 없는 수 찾기
 //		아래 배열에 1 ~ 9 의 숫자들이 들어있는데 하나의 수가 빠져 있다.
@@ -87,18 +82,12 @@ public class Test03 {
 //		없는 수는 7 입니다 
 		
 		System.out.println("아래 배열에 1 ~ 9 의 숫자들이 들어있는데 하나의 수가 빠져 있다.\r\n"
-				+ "//		빠진 수를 찾아서 출력하세요.");
-		int[] numbers2 = {4, 6, 3, 8, 2, 9, 1, 5};
+				+ "빠진 수를 찾아서 출력하세요.");
+		int[] numbers3 = {4, 6, 3, 8, 2, 9, 1, 5};
 		
-		int[] numbers3 = {1,2,3,4,5,6,7,8,9};
-		
-		for(int i = 0 ; i < numbers2.length; i++) {
-			for(int j = 0;j < numbers3.length; j++) {
-				if(numbers2[i] == numbers3[j]) {
-					
-				}else {
-					
-				}
+		for(int i = 0 ; i < numbers3.length; i++) {
+			if(numbers3[i] == 1) {
+				
 			}
 		}
 		
@@ -115,6 +104,48 @@ public class Test03 {
 //		3 : 3
 //		4 : 1
 //		5 : 1
+		
+		System.out.println("1 ~ 5 까지의 숫자가 각각 몇 개씩 저장 되어 있는지 출력 하세요.");
+		
+		int[] numbers4 = {5, 3, 2, 1, 2, 4, 3, 3, 2, 1};
+		
+		int[] frequency = new int [5];
+		
+		for(int i = 0; i < numbers4.length; i++) {
+			int number = numbers4[i];
+			frequency[number - 1]++;
+		}
+		
+		for (int i = 0; i < frequency.length; i++) {
+			System.out.println((i + 1) + " : " + frequency[i]);
+		}
+		
+		
+//		int count1 = 0;
+//		int count2 = 0;
+//		int count3 = 0;
+//		int count4 = 0;
+//		int count5 = 0;
+//		
+//		for(int i = 0; i < numbers4.length; i++) {
+//			if(numbers4[i] == 1) {
+//				count1++;
+//			}else if(numbers4[i] == 2) {
+//				count2++;
+//			}else if(numbers4[i] == 3) {
+//				count3++;
+//			}else if(numbers4[i] == 4) {
+//				count4++;
+//			}else if(numbers4[i] == 5) {
+//				count5++;
+//			}
+//		}
+//		
+//		System.out.println("1: " + count1);
+//		System.out.println("2: " + count2);
+//		System.out.println("3: " + count3);
+//		System.out.println("4: " + count4);
+//		System.out.println("5: " + count5);
 		
 	}
 
