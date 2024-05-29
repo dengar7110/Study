30969fbf4cdec1d08254ba4a6d4a7a0cf2f1f510
garden <124008155+dengar7110@users.noreply.github.com>
2024-05-29 20:23:54 +0900

    Update Test02.java
    0529

--- DemensionalArray/test/Test02.java
+++ DemensionalArray/test/Test02.java
@@ -3,8 +3,6 @@ package test;
 public class Test02 {
 
 	public static void main(String[] args) {
-		
-		
 //		1. 10 입력하기
 //		[2][3] 크기의 배열을 만드세요.
 //		반복문을 이용해서, 모든 곳에 10을 넣고 출력하세요.
@@ -15,20 +13,15 @@ public class Test02 {
 		
 		System.out.println("반복문을 이용해서, 모든 곳에 10을 넣고 출력하세요.");
 		
-		int request[][] = new int[2][3];
+		int[][] numbers = new int[2][3];
 		
-		for(int i = 0; i < request.length; i++) {
-			for(int j = 0; j < request[i].length; j++) {
-				request[i][j] = 10;
+		for(int i = 0; i < numbers.length; i++) {
+			for(int j = 0; j < numbers[i].length; j++) {
+				numbers[i][j] = 10;
 			}
 		}
 		
-		for(int i = 0; i < request.length; i++) {
-			for(int j = 0; j < request[i].length; j++) {
-				System.out.print(request[i][j] + " ");
-			}
-			System.out.println();
-		}
+		print2DArray(numbers);
 		
 		
 //		2. 순서대로 입력하기
@@ -42,19 +35,15 @@ public class Test02 {
 		
 		System.out.println("반복문을 이용해서, 아래와 같이 각 배열에 1~4를 저장 하고, 출력하세요.");
 		
-		int request2[][] = new int[3][4];
-		for(int i = 0; i <  request2.length; i++) {
-			for(int j = 0; j < request2[i].length; j++) {
-				request2[i][j] = (j + 1);
+		int[][] numbers2 = new int[3][4];
+		
+		for(int i = 0; i < numbers2.length; i++) {
+			for(int j = 0; j < numbers2[i].length; j++){
+				numbers2[i][j] = (j + 1) ;
 			}
 		}
 		
-		for(int i = 0; i < request2.length; i++) {
-			for(int j = 0; j < request2[i].length; j++) {
-				System.out.print(request2[i][j] + " ");
-			}
-			System.out.println();
-		}
+		print2DArray(numbers2);
 		
 //		3. 배열 별로 입력하기
 //		[3][3] 크기의 배열을 만드세요.
@@ -67,20 +56,16 @@ public class Test02 {
 		
 		System.out.println("반복문을 이용해서, 첫 배열은 1, 두번째 배열은 2, 세번째 배열은 3 으로 채우고 출력 하세요.");
 		
-		int request3[][] = new int[3][3];
-		for(int i = 0; i < request3.length; i++) {
-			for(int j = 0; j < request3[i].length; j++) {
-				request3[i][j] = (i + 1);
+		int[][] numbers3 = new int[3][3];
+		
+		for(int i = 0; i < numbers3.length; i++) {
+			for(int j = 0; j < numbers3[i].length; j++){
+				numbers3[i][j] = (i + 1) ;
 			}
 		}
-
-		for(int i = 0; i < request3.length; i++) {
-			for(int j = 0; j < request3[i].length; j++) {
-				System.out.print(request3[i][j] + " ");
-			}
-			System.out.println();
-		}
-
+		
+		print2DArray(numbers3);
+		
 		
 		
 //		4. +자 그리기
@@ -94,32 +79,21 @@ public class Test02 {
 //		1 1 1 1 1 
 //		0 0 1 0 0 
 //		0 0 1 0 0 
-		
 		System.out.println("반복문을 이용해서, 아래와 같이 + 위치에 1을 입력 하고 출력하세요.");
 		
-		int request4[][] = new int[5][5];
-		
-		for(int i = 0; i < request4.length; i++) {
-			for(int j = 0; j < request4[i].length; j++) {
-				request4[i][j] = 0;
-			}
-		}
-		
-		for(int i = 0; i < request4.length; i++) {
-			for(int j = 0; j < request4[i].length; j++) {
+		int[][] numbers4 = new int[5][5];
+				
+		for(int i = 0; i < numbers4.length; i++) {
+			for(int j = 0; j < numbers4[i].length; j++){
+				
 				if(i == 2 || j == 2) {
-					request4[i][j] = 1;
+					numbers4[i][j] = 1;
 				}
 
 			}
 		}
-		
-		for(int i = 0; i < request4.length; i++) {
-			for(int j = 0; j < request4[i].length; j++) {
-				System.out.print(request4[i][j] + " ");
-			}
-			System.out.println();
-		}
+				
+		print2DArray(numbers4);
 		
 //		5. 전체 수 입력
 //		[3][3] 크기의 배열을 만드세요.
@@ -132,21 +106,17 @@ public class Test02 {
 		
 		System.out.println("반복문을 이용해서, 아래와 같은 형태로 숫자를 차례로 저장하고 출력 하세요.");
 		
-		int request5[][] = new int[3][3];
+		int [][] numbers5 = new int[3][3];
+		
 		int number = 1;
-		for(int i = 0; i < request5.length; i++) {
-			for(int j = 0; j < request5.length; j++) {
-				request5[i][j] = number;
+		for(int i = 0; i < numbers5.length; i++) {
+			for(int j = 0; j < numbers5[i].length; j++) {
+				numbers5[i][j] = number;
 				number++;
 			}
 		}
 		
-		for(int i = 0; i < request5.length; i++) {
-			for(int j = 0; j < request5.length; j++) {
-				System.out.print(request5[i][j] + " ");
-			}
-			System.out.println();
-		}
+		print2DArray(numbers5);
 		
 //		6. 전체 수 입력
 //		[3][3] 크기의 배열을 만드세요.
@@ -156,26 +126,37 @@ public class Test02 {
 //		1 4 7 
 //		2 5 8 
 //		3 6 9 
-		
-		int request6[][] = new int[3][3];
-		
+
 		System.out.println("반복문을 이용해서, 아래와 같은 형태로 숫자를 차례로 저장하고 출력 하세요.");
 		
+		int [][] numbers6 = new int[3][3];
+		
 		number = 1;
-		for(int i = 0; i < request6.length; i++) {
-			for(int j = 0; j < request6[i].length; j++) {
-				request6[j][i] = number;
+		
+		for(int i = 0; i < numbers6.length; i++) {
+			for(int j = 0; j < numbers6[i].length; j++) {
+				numbers6[j][i] = number;
 				number++;
 			}
+			
 		}
 		
-		for(int i = 0; i < request6.length; i++) {
-			for(int j = 0; j < request6[i].length; j++) {
-				System.out.print(request6[i][j] + " ");
+		print2DArray(numbers6);
+		
+		
+		
+	}
+	// 2차원 배열을 출력하는 함수
+	public static void print2DArray(int array[][]) {
+		
+		for(int i = 0; i < array.length; i++) {
+			for(int j = 0; j < array[i].length; j++) {
+				System.out.print(array[i][j] + " ");
 			}
 			System.out.println();
 		}
 		
 	}
-
+	
+	
 }
