332471172b62d708524e68216027860127460cb6
garden <124008155+dengar7110@users.noreply.github.com>
2024-06-03 18:02:26 +0900

    Update 이어붙인수.java
    0603

--- CodingTest/test/이어붙인수.java
+++ CodingTest/test/이어붙인수.java
@@ -1,26 +1,25 @@
-package CodingTest;
+package test;
 
 public class 이어붙인수 {
-//	정수가 담긴 리스트 num_list가 주어집니다. num_list의 홀수만 순서대로 이어 붙인 수와 짝수만 순서대로 이어 붙인 수의 합을 return하도록 solution 함수를 완성해주세요.
-	
-	class Solution {
-		public int solution(int[] num_list) {
-	        StringBuilder oddConcat = new StringBuilder(); // 홀수를 이어붙일 StringBuilder
-	        StringBuilder evenConcat = new StringBuilder(); // 짝수를 이어붙일 StringBuilder
 
-	        // 주어진 리스트를 순회하면서 홀수와 짝수를 구분하여 이어붙임
-	        for (int num : num_list) {
-	            if (num % 2 == 0) { // 짝수인 경우
-	                evenConcat.append(num);
-	            } else { // 홀수인 경우
-	                oddConcat.append(num);
-	            }
-	        }
-          // 이어붙인 홀수와 짝수를 정수로 변환하여 합을 계산하여 반환
-         int oddSum = Integer.parseInt(oddConcat.toString());
-         int evenSum = Integer.parseInt(evenConcat.toString());
-         return oddSum + evenSum;
-         }
-	}   
+	class Solution {
+	    public int solution(int[] num_list) {
+	        String evensum = "";
+	        String oddsum = "";
+	       
+	    	for(int i = 0; i < num_list.length; i++) {
+	    		if(num_list[i] % 2 == 0) {
+	    			evensum += num_list[i];
+	    		}else {
+	    			oddsum += num_list[i];
+	    		}
+	    		
+	    	}
+	    	
+	    	int answer = Integer.parseInt(evensum) + Integer.parseInt(oddsum);
+	        return answer;
+	    }
+	}
 	
 }
+
