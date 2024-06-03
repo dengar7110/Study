9f2e5482933b00bf889a99eff52bd9647e2b707e
garden <124008155+dengar7110@users.noreply.github.com>
2024-06-03 20:02:27 +0900

    Update Circle.java
    0603

--- Class/test/test02/Circle.java
+++ Class/test/test02/Circle.java
@@ -2,24 +2,28 @@ package test.test02;
 
 public class Circle {
 
+	// 멤버변수
 	int radius;
+	int count;
 	
-	double getArea(int radius) {
+//	메소드는 아래 를 참조하세요
+//	넓이 구하기, 둘레 구하기, 개수에 따른 넓이 구하기
+	
+	double calculateArea() {
 		double area = 3.14 * radius * radius;
-		
 		return area;
 	}
 	
-	double getRound(int radius) {
+	double calculateRound() {
 		double round = 2 * 3.14 * radius;
-		
 		return round;
 	}
 	
-	double get25Area(int radius) {
-		double area25 = 3.14 * radius * radius * 25;
-		
-		return area25;
+	// overload - 같은 이름의 파라미터가 다른 메소드
+	// 파라미터 개수, 파라미터 타입
+	double calculateArea(int count) {
+		double area = calculateArea() * count;
+		return area;
 	}
 	
 	
