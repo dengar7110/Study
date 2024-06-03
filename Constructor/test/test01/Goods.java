8161b116612de31c643c719a35905b4cfa674c94
garden <124008155+dengar7110@users.noreply.github.com>
2024-06-03 21:49:48 +0900

    Update Goods.java
    0603

--- Constructor/test/test01/Goods.java
+++ Constructor/test/test01/Goods.java
@@ -4,12 +4,12 @@ public class Goods {
 
 	private String name;
 	private int price;
-	private String expirationDate;
+	private String expiration;
 	
-	public Goods(String name , int price, String expirationDate){
+	public Goods(String name , int price, String expiration){
 		this.name = name;
 		this.price = price;
-		this.expirationDate = expirationDate;
+		this.expiration = expiration;
 	}
 	
 	public int calculatePrice(int count) {
@@ -19,7 +19,7 @@ public class Goods {
 	public void printInfo() {
 		System.out.println("이름 : " + name );
 		System.out.println("가격 : " + price );
-		System.out.println("유통기한 : " + expirationDate );
+		System.out.println("유통기한 : " + expiration );
 	}
 	
 	public boolean availableSale(String today) {
@@ -27,9 +27,9 @@ public class Goods {
 //		int todayMonth = Integer.parseInt(today.substring(5,7));
 //		int todayDay = Integer.parseInt(today.substring(8));
 //		
-//		int expirationYear = Integer.parseInt(expirationDate.substring(0,4));
-//		int expirationMonth = Integer.parseInt(expirationDate.substring(5,7));
-//		int expirationDay = Integer.parseInt(expirationDate.substring(8));
+//		int expirationYear = Integer.parseInt(expiration.substring(0,4));
+//		int expirationMonth = Integer.parseInt(expiration.substring(5,7));
+//		int expirationDay = Integer.parseInt(expiration.substring(8));
 //		
 //		if(todayYear > expirationYear || (todayYear == expirationYear && todayMonth > expirationMonth) || (todayYear == expirationYear && todayMonth == expirationMonth && todayDay > expirationDay)) {
 //			return false;
@@ -38,28 +38,47 @@ public class Goods {
 //		
 //		return true;
 		
-		// "2024-09-12"
-		String[] expirationArray = expirationDate.split("-");
-		String[] todayArray = today.split("-");
+		// split 사용
+//		String[] expirationArray = expiration.split("-");
+//		String[] todayArray = today.split("-");
+//		
+//		int expirationYear = Integer.parseInt(expirationArray[0]);
+//		int expirationMonth = Integer.parseInt(expirationArray[1]);
+//		int expirationDay = Integer.parseInt(expirationArray[2]);
+//		
+//		int todayYear = Integer.parseInt(todayArray[0]);
+//		int todayMonth = Integer.parseInt(todayArray[1]);
+//		int todayDay = Integer.parseInt(todayArray[2]);
+//		
+//		if(expirationYear > todayYear) {
+//			return true;
+//		}else if(expirationYear == todayYear && expirationMonth > todayMonth) {
+//			return true;
+//		}else if(expirationYear == todayYear && expirationMonth == todayMonth && expirationDay >= todayDay) {
+//			return true;
+//		}else {
+//			return false;
+//		}
+//		
 		
-		int expirationYear = Integer.parseInt(expirationArray[0]);
-		int expirationMonth = Integer.parseInt(expirationArray[1]);
-		int expirationDay = Integer.parseInt(expirationArray[2]);
+		// replace 사용 
+//		String expirationDate = expiration.replace("-", "");
+//		String todayDate = today.replace("-", "");
+//		
+//		int expirationInt = Integer.parseInt(expirationDate);
+//		int todayInt = Integer.parseInt(todayDate);
+//		
+//		if(expirationInt >= todayInt) {
+//			return true;
+//		}else {
+//			return false;
+//		}
 		
-		int todayYear = Integer.parseInt(todayArray[0]);
-		int todayMonth = Integer.parseInt(todayArray[1]);
-		int todayDay = Integer.parseInt(todayArray[2]);
-		
-		if(expirationYear > todayYear) {
-			return true;
-		}else if(expirationYear == todayYear && expirationMonth > todayMonth) {
-			return true;
-		}else if(expirationYear == todayYear && expirationMonth == todayMonth && expirationDay >= todayDay) {
+		if(expiration.compareTo(today) >= 0) {
 			return true;
 		}else {
 			return false;
 		}
-		
 		
 	}
 	
