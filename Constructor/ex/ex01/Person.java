ed0b574d1f6657dbe385207b9478354d8d31b91d
garden <124008155+dengar7110@users.noreply.github.com>
2024-06-04 19:37:02 +0900

    Update Person.java
    0604

--- Constructor/ex/ex01/Person.java
+++ Constructor/ex/ex01/Person.java
@@ -16,6 +16,26 @@ public class Person {
 		this.birthday = birthday;
 	}
 	
+	public String getName() {
+		return name;
+	}
+	
+	// name 값 수정 기능
+	// setter
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	// birthday 값 리턴 기능
+	// getter
+	public String getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+
 	// 정보 출력 기능
 	public void printInfo() {
 		System.out.println("이름 : " + name + " 생년월일 : " + birthday);
