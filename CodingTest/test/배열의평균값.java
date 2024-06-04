6c6275c157aa19c4f9b1e5f10bb8af2b58c2fe55
garden <124008155+dengar7110@users.noreply.github.com>
2024-06-04 18:16:53 +0900

    Create 배열의평균값.java
    0604

---
+++ CodingTest/test/배열의평균값.java
@@ -0,0 +1,15 @@
+package test;
+
+public class 두수의합 {
+	class Solution {
+	    public double solution(int[] numbers) {
+	        int sum = 0;
+	        for(int i = 0; i < numbers.length; i++){
+	            sum += numbers[i];
+	        }
+	        
+	        double answer = sum / (double)numbers.length;
+	        return answer;
+	    }
+	}
+}
