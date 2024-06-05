ccffdd353daa9b277c9f328d6577e9e2bc0e30a9
garden <124008155+dengar7110@users.noreply.github.com>
2024-06-05 18:28:48 +0900

    Update 옷가게할인받기.java
    0605

--- CodingTest/test/옷가게할인받기.java
+++ CodingTest/test/옷가게할인받기.java
@@ -5,12 +5,14 @@ public class 옷가게할인받기 {
 	class Solution {
 	    public int solution(int price) {
 	        int answer = 0;
-	        if(price >= 100000 && price < 300000) {
-	        	answer = (price / 100) * 95; 
-	        }else if(price >= 300000 && price < 500000) {
-	        	answer = (price / 100) * 90; 
-	        }else if(price >= 500000) {
-	        	answer = (price / 100) * 80; 
+	        if(price >= 500000) {
+	            answer = price * 80 / 100; // 20% 할인
+	        } else if(price >= 300000) {
+	            answer = price * 90 / 100; // 10% 할인
+	        } else if(price >= 100000) {
+	            answer = price * 95 / 100; // 5% 할인
+	        } else {
+	            answer = price; // 할인 없음
 	        }
 	        
 	        
