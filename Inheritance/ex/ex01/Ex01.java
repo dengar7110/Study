0d5a605252740ef53af243dcefd961a5f4a102a5
garden <124008155+dengar7110@users.noreply.github.com>
2024-06-05 21:10:32 +0900

    Create Ex01.java
    0605

---
+++ Inheritance/ex/ex01/Ex01.java
@@ -0,0 +1,14 @@
+package ex.ex01;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+
+		Student student = new Student("홍정원", "20030923", "낙성대학교");
+		
+		student.printInfo();
+		
+		
+	}
+
+}
