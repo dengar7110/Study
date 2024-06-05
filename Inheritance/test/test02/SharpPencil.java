6523c2b6c4f3666983ad5cc77e5a7384148dd5f6
garden <124008155+dengar7110@users.noreply.github.com>
2024-06-05 21:49:20 +0900

    Create SharpPencil.java
    0605

---
+++ Inheritance/test/test02/SharpPencil.java
@@ -0,0 +1,14 @@
+package test.test02;
+
+class SharpPencil { // 샤프펜슬
+    private int width; // 펜의 굵기
+    private int amount; // 남은 량
+
+    public int getAmount() { 
+        return amount; 
+    }
+
+    public void setAmount (int amount) {
+        this.amount = amount; 
+    }
+}
