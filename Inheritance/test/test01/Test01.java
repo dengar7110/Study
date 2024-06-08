e1e7b0e518ebed35e681e058013f84832edb276c
garden <124008155+dengar7110@users.noreply.github.com>
2024-06-08 21:28:54 +0900

    Update Test01.java
    0608

--- Inheritance/test/test01/Test01.java
+++ Inheritance/test/test01/Test01.java
@@ -1,7 +1,9 @@
 package test.test01;
 
 public class Test01 {
+
 	public static void main(String[] args) {
+		
 		//	iPod Touch Class 를 상속 받은 iPhone Class 를 설계하세요.
 		//	iPhone 에는 아래와 같은 추가 기능이 포함되어 있습니다.
 		//	전화 하기, 메시지 보내기
@@ -12,11 +14,15 @@ public class Test01 {
 		//	로딩 완료
 		//	010-000-0000 로 통화 중
 		//	010-1111-2222 로 ㅋㅋㅋㅋ 전송
-	
-		IPhone iphone = new IPhone("010-1234-5678");
-		System.out.println(iphone.infoString());
-		iphone.internetBrowsing("https://www.google.com");
-		iphone.call("010-000-0000");
-		iphone.message("010-1111-2222", "ㅋㅋㅋㅋ");
+		
+		IPhone myPhone = new IPhone("010-1234-5678");
+		
+		System.out.println(myPhone.infoString());
+		myPhone.internetBrowsing("https://www.google.com");
+		
+		myPhone.call("010-0000-0000");
+		myPhone.sendMessage("010-1111-2222", "ㅋㅋㅋㅋ");
+		
 	}
+	
 }
