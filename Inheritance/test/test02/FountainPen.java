ef352772113b10617f956578f414e69dfeccb13a
garden <124008155+dengar7110@users.noreply.github.com>
2024-06-08 21:27:28 +0900

    Update FountainPen.java
    0608

--- Inheritance/test/test02/FountainPen.java
+++ Inheritance/test/test02/FountainPen.java
@@ -1,32 +1,9 @@
 package test.test02;
 
-class FountainPen extends Ballpen{ // 만년필
-//    private int amount; // 남은 량
-//    private String color; // 볼펜의 색
-    
-    // override SharpPencil
-    @Override
-    public int getAmount() { 
-        return super.getAmount(); 
-    }
-    // override SharpPencil
-    @Override
-    public void setAmount (int amount) { 
-    	super.setAmount(amount);
-    }
-    
-    // override Ballpen
-    @Override
-    public String getColor() { 
-        return super.getColor(); 
-    }
-    
-    // override Ballpen
-    @Override
-    public void setColor(String color) { 
-       super.setColor(color);
-    }
-    
+class FountainPen extends Ballpen { // 만년필
+	
+    private String color; // 볼펜의 색
+
     public void refill (int n) { 
         setAmount(n); 
     }
