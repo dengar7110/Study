aa88686fb750abd6cc81d7f5516697458d498b9f
garden <124008155+dengar7110@users.noreply.github.com>
2024-06-08 21:29:47 +0900

    Create Ex01.java
    0608

---
+++ Object/ex/Ex01.java
@@ -0,0 +1,17 @@
+package ex;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		
+		Person me = new Person("홍정원", 29);
+		
+		me.printInfo();
+		
+//		System.out.println(me.toString());
+		System.out.println(me);
+		
+		
+		
+	}
+}
