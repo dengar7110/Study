fbdc6e528e38c22c4f1c2ed373e987ea8abe9570
garden <124008155+dengar7110@users.noreply.github.com>
2024-06-10 20:18:43 +0900

    Update Rectangle.java
    0610

--- Polymorphism/test/test02/Rectangle.java
+++ Polymorphism/test/test02/Rectangle.java
@@ -1,18 +1,32 @@
 package test.test02;
 
 public class Rectangle extends Shape {
-
+	
+	// 가로, 세로
 	private int width;
 	private int height;
 	
 	public Rectangle(int width, int height) {
+		this.name = "사각형";
 		this.width = width;
 		this.height = height;
 	}
 	
 	@Override
+	public String toString() {
+		return "가로 : " + width + "세로 : " + height; 
+	}
+	
+	// 넓이를 구하는 기능
+	@Override
 	public double calculateArea() {
 		return width * height;
+	}
+	
+	// 넓이를 출력하는 기능
+	@Override
+	public void printArea() {
+		System.out.println("사각형의 넓이는 : " + calculateArea() +  "cm^2 입니다.");
 	}
 	
 }
