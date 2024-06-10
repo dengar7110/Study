25fbb7cf4555a8e7f480d7ebc7cfe39888c63090
garden <124008155+dengar7110@users.noreply.github.com>
2024-06-10 20:52:38 +0900

    Update Lotto.java
    0610

--- Constructor/test/test04/Lotto.java
+++ Constructor/test/test04/Lotto.java
@@ -4,52 +4,50 @@ import java.util.Random;
 
 public class Lotto {
 
-	private int[] manualLotto;
-	private int[] autoLotto;
+	// 선택된 번호
+	private int[] checkNumbers = new int[6];
+
+	// 수동 선택
+	public void manual(int number1, int number2, int number3, int number4, int number5, int number6) {
+		checkNumbers[0] = number1;
+		checkNumbers[1] = number2;
+		checkNumbers[2] = number3;
+		checkNumbers[3] = number4;
+		checkNumbers[4] = number5;
+		checkNumbers[5] = number6;
+	}
 	
-	public Lotto(int num1, int num2, int num3, int num4, int num5, int num6){
-		manualLotto = new int[] {num1, num2, num3, num4, num5, num6};
+	public void auto() {
+		// 임의의 숫자 얻기
+		// 1 ~ 45 범위의 숫자
 		
-		
-		autoLotto = new int[6];
-		Random ran = new Random();
-		
-		for(int i = 0; i < autoLotto.length;i++) {
-			int random = ran.nextInt(46);
-			autoLotto[i] = random;
+		Random random = new Random();
+		for(int i = 0; i < checkNumbers.length; i++) {
+			int randomNumber =  random.nextInt(45) + 1; // 0 ~ 44
+			checkNumbers[i] = randomNumber;
+			
+			for(int j = 0; j < i; j++) {
+				if(randomNumber == checkNumbers[j]) {
+					// 중복되었다
+					i--;
+					break;
+				}
+			}
 		}
 	}
 	
-	public int[] getManualLotto() {
-		return manualLotto;
+	public void printInfo() {
+		System.out.print("선택한 번호 : ");
+		for(int i = 0; i < checkNumbers.length; i++) {
+			System.out.print(checkNumbers[i] + " ");
+		}
+		System.out.println();
 	}
 
-	public int[] getAutoLotto() {
-		return autoLotto;
+	public int[] getCheckNumbers() {
+		return checkNumbers;
 	}
 	
-	public void setManual(int num1, int num2, int num3, int num4, int num5, int num6) {
-		manualLotto = new int[] {num1, num2, num3, num4, num5, num6};
-	}
-	
-	
-	public String strManual() {
-		String manual = "";
-		for(int i = 0; i < manualLotto.length; i++) {
-			manual += manualLotto[i] + " ";
-		}
-		
-		return manual;
-	}
 
-	public String strAuto() {
-		String auto ="";
-		for(int i = 0; i < autoLotto.length; i++) {
-			auto += autoLotto[i] + " ";
-		}
-		
-		return auto;
-	}
-	
-	
+
 }
