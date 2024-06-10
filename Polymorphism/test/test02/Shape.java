2b548cf0cd602e4d7cc2229e604b56a9846c8e24
garden <124008155+dengar7110@users.noreply.github.com>
2024-06-10 20:18:54 +0900

    Update Shape.java
    0610

--- Polymorphism/test/test02/Shape.java
+++ Polymorphism/test/test02/Shape.java
@@ -2,9 +2,17 @@ package test.test02;
 
 public class Shape {
 
+	// 도형이름
+	protected String name;
+	
 	public double calculateArea() {
+		// nothing
 		return 0;
-		
 	}
+
+	public void printArea() {
+		System.out.println(name + "의 넓이는 " + calculateArea() + "cm^2입니다.");
+	}
+	
 	
 }
