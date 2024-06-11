0435cb93a59c05801596c71a74e709bf973a19e3
garden <124008155+dengar7110@users.noreply.github.com>
2024-06-11 20:32:23 +0900

    Create JypEntertainment.java
    0611

---
+++ Interface/test/JypEntertainment.java
@@ -0,0 +1,13 @@
+package test;
+
+public class JypEntertainment {
+
+	public void actorAudition(Act applicant) {
+		applicant.act();
+	}
+	
+	public void idolAudition(Sing applicant) {
+		applicant.sing();
+	}
+	
+}
