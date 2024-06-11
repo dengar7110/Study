c7db19db42a5f680ef72b2524345553e289dfb7d
garden <124008155+dengar7110@users.noreply.github.com>
2024-06-11 20:43:09 +0900

    Update JypEntertainment.java
    0611

--- Interface/test/JypEntertainment.java
+++ Interface/test/JypEntertainment.java
@@ -2,12 +2,18 @@ package test;
 
 public class JypEntertainment {
 
+	// 소속 연예인수, 영업이익
+	private int artistCount; 
+	private int operatingProfit;
+	
+	// 연기자 오디션
 	public void actorAudition(Act applicant) {
-		applicant.act();
+		applicant.methodActing();
 	}
 	
+	// 아이돌 오디션
 	public void idolAudition(Sing applicant) {
-		applicant.sing();
+		applicant.singASong();
 	}
 	
 }
