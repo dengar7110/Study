2fbb114f18a42fecba201c41ed2377de5911771d
garden <124008155+dengar7110@users.noreply.github.com>
2024-06-14 21:45:31 +0900

    Update Ex01.java
    0614

--- Map/ex/Ex01.java
+++ Map/ex/Ex01.java
@@ -2,6 +2,7 @@ package ex;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class Ex01 {
 	
@@ -20,6 +21,49 @@ public class Ex01 {
 		memberMap.put("ohndar","홍정원");
 		
 		System.out.println(memberMap);
+		
+		// 값 가져오기
+		String name = memberMap.get("yu");
+		System.out.println(name);
+		
+		// 값 수정
+		// 바다 -> 김바다
+		memberMap.put("bada", "김바다");
+		System.out.println(memberMap);
+		
+		// 특정키가 존재하는지 확인
+		
+		// get() 대응되는 key가 없을 경우 null 을 리턴
+		String result = memberMap.get("dulumary");
+		System.out.println(result);
+//		result.length();
+		
+		String tmp = null;
+
+		// 특정키가 존재하는지 확인
+		if(memberMap.containsKey("yu")) {
+			System.out.println(memberMap.get("yu"));
+		}
+
+		// 맵 반복문
+		// 자주 사용되지 않음
+		
+		// 키의 목록을 얻어 온다.
+		// 키들의 목록을 반복문으로 키를 하나씩 얻어 온다.
+		// 키를 통해 값을 얻어온다.
+		
+		// 키 목록 얻어오기
+		Set<String> KeySet = memberMap.keySet();
+		
+		for(String id : KeySet) {
+			
+			String value = memberMap.get(id);
+			System.out.println(value);
+			
+		}
+		
+		
+		
 		
 	}
 	
