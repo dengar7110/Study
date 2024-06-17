d1afaa92132d685a18a89f46239863b180e85069
garden <124008155+dengar7110@users.noreply.github.com>
2024-06-17 21:08:30 +0900

    Create ThreadImplements.java
    0617

---
+++ Thread/ex/ThreadImplements.java
@@ -0,0 +1,26 @@
+package ex;
+
+public class ThreadImplements implements Runnable {
+
+	private String message;
+	
+	@Override
+	public void run() {
+		for(int i = 0; i < 10; i++) {
+			
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+			System.out.println(message);
+		}
+	}
+	
+	
+	public ThreadImplements(String message) {
+		this.message = message;
+	}
+	
+}
