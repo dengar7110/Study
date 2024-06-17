e2395014e6b62b2993717e3e71ab1989edc95955
garden <124008155+dengar7110@users.noreply.github.com>
2024-06-17 21:08:58 +0900

    Create Zoo.java
    0617

---
+++ Thread/test/Zoo.java
@@ -0,0 +1,26 @@
+package test;
+
+public class Zoo implements Runnable {
+
+	public String sound;
+	
+	public Zoo(String sound) {
+		this.sound = sound;
+	}
+	
+	@Override
+	public void run() {
+		
+		for(int i = 0; i < 5; i++) {
+			
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println(sound);
+		}
+		
+	}
+}
