f14dcfb8c9c9d7c2b715b2ecf60763717e2eca84
garden <124008155+dengar7110@users.noreply.github.com>
2024-06-17 21:20:42 +0900

    Update Test01.java
    0617

--- Thread/test/Test01.java
+++ Thread/test/Test01.java
@@ -4,28 +4,25 @@ public class Test01 {
 
 	public static void main(String[] args) {
 		
-		Zoo tiger = new Zoo("어흥");
-		Zoo cow = new Zoo("음메~");
-		Zoo horse = new Zoo("히잉");
+		Animal tiger = new Animal("어흥");
+		Animal cow = new Animal("음메~");
+		Animal horse = new Animal("히잉");
 		
-		Thread thread1 = new Thread(tiger);
-		Thread thread2 = new Thread(cow);
-		Thread thread3 = new Thread(horse);
+		Thread tigerThread = new Thread(tiger);
+		Thread cowThread = new Thread(cow);
+		Thread horseThread = new Thread(horse);
 		
 		
-		thread1.start();
-		thread2.start();
+		tigerThread.start();
+		cowThread.start();
 		try {
-			thread1.join();
-			thread2.join();
+			tigerThread.join();
+			cowThread.join();
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		thread3.start();
 		
-
-		
-		
+		horseThread.start();
 		
 	}
 	
