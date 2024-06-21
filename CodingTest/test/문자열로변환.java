59982a96864b75b6fc02b82202f0c12ff631fde4
garden <124008155+dengar7110@users.noreply.github.com>
2024-06-21 18:04:32 +0900

    Create 문자열로변환.java
    0621

---
+++ CodingTest/test/문자열로변환.java
@@ -0,0 +1,15 @@
+package test;
+
+public class 문자열로변환 {
+
+//	정수 n이 주어질 때, n을 문자열로 변환하여 return하도록 solution 함수를 완성해주세요.
+
+	class Solution {
+	    public String solution(int n) {
+	        String answer = "";
+	        answer = String.valueOf(n);
+	        return answer;
+	    }
+	}
+	
+}
