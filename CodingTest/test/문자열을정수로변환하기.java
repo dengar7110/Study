884e62610f1dc08d3cb901b69f4e82f644f49960
garden <124008155+dengar7110@users.noreply.github.com>
2024-06-21 18:30:45 +0900

    Create 문자열을정수로변환하기.java
    0621

---
+++ CodingTest/test/문자열을정수로변환하기.java
@@ -0,0 +1,16 @@
+package test;
+
+public class 문자열을정수로변환하기 {
+
+//	숫자로만 이루어진 문자열 n_str이 주어질 때,
+//	n_str을 정수로 변환하여 return하도록 solution 함수를 완성해주세요.
+	
+	class Solution {
+	    public int solution(String n_str) {
+	        int answer = 0;
+	        answer = Integer.parseInt(n_str);
+	        return answer;
+	    }
+	}
+	
+}
