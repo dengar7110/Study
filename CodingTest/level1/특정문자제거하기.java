85a784b824310c16867c5f691ec4a98402921393
garden <124008155+dengar7110@users.noreply.github.com>
2024-07-04 18:36:12 +0900

    Create 특정문자제거하기.java
    0704

---
+++ CodingTest/level1/특정문자제거하기.java
@@ -0,0 +1,18 @@
+package level1;
+
+public class 특정문자제거하기 {
+	
+//	문자열 my_string과 문자 letter이 매개변수로 주어집니다.
+//	my_string에서 letter를 제거한 문자열을 return하도록 solution 함수를 완성해주세요.
+
+	class Solution {
+	    public String solution(String my_string, String letter) {
+	        String answer = "";
+	        
+	        answer = my_string.replaceAll(letter, "");
+	        
+	        return answer;
+	    }
+	}
+
+}
