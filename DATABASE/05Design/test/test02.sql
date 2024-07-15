cf10d5be47dc0fa881b9e1c6524e0485f4dbad82
garden <124008155+dengar7110@users.noreply.github.com>
2024-07-15 19:40:11 +0900

    Update test02.sql
    0715

--- DATABASE/05Design/test/test02.sql
+++ DATABASE/05Design/test/test02.sql
@@ -16,12 +16,13 @@
 -- 가게 소개(introduce)
 -- 중복데이터를 줄이고 효율적인 방법으로 테이블을 나누어서 설계하세요.
 
-CREATE TABLE `new_review` (
+-- 가게 정보
+CREATE TABLE `store` (
 	`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `store` VARCHAR(16) NOT NULL,
+    `store` VARCHAR(32) NOT NULL,
     `phoneNumber` VARCHAR(16) NOT NULL,
-    `address` VARCHAR(16) NOT NULL,
-    `businessNumber` VARCHAR(16) NOT NULL,
+    `address` VARCHAR(64) NOT NULL,
+    `businessNumber` VARCHAR(32) NOT NULL,
     `introduce` TEXT,
     `createdAt` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     `updatedAt` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
@@ -29,34 +30,38 @@ CREATE TABLE `new_review` (
 
 DROP TABLE `new_review`;
 
-CREATE TABLE `review_review` (
+-- 리뷰 정보
+CREATE TABLE `new_review` (
 	`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `userId` INT NOT NULL,
-    `menu` VARCHAR(16) NOT NULL,
-    `userName` VARCHAR(16) NOT NULL,
-    `point` DECIMAL(2,1) NOT NULL,
-	`review` TEXT,
+    `storeId` INT NOT NULL,
+    `menu` VARCHAR(32) NOT NULL,
+    `userName` VARCHAR(32) NOT NULL,
+    `point` DECIMAL(2, 1) DEFAULT 3.5,
+	`review` VARCHAR(256),
 	`createdAt` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     `updatedAt` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-DROP TABLE `review_review`;
+DROP TABLE `new_review`;
+
+DESC `store`;
+DESC `new_review`;
 
 
 -- 2. Insert
 -- 위에 표에 표시된 데이터를 나누어진 테이블에 저장하세요.
 
-INSERT INTO `new_review`
+INSERT INTO `store`
 (`store`, `phoneNumber`, `address`, `businessNumber`, `introduce`)
 VALUES
 ('교촌치킨', '02-000--0000', '서울시 서초구', '111-111-1111', '항상 맛있는 닭을 선사 합니다.'),
 ('BHC', '02-111-0000', '서울시 서초구', '111-222-1111', '전지현씨 BHC'),
 ('버거킹', '02-222-1111', '서울시 서초구', '333-222-12222', '맥도날드 가지말고 여기로와요');
 
-SELECT * FROM `new_review`;
+SELECT * FROM `store`;
 
-INSERT INTO `review_review`
-(`userId`, `menu`, `userName`, `point`, `review`)
+INSERT INTO `new_review`
+(`storeId`, `menu`, `userName`, `point`, `review`)
 VALUES
 (1 ,'오리지날 콤보', '이병헌', 4.5, '콤보는 항상 진리입니다.'),
 (2, '핫후라이드', '전지현', 5, NULL),
@@ -65,7 +70,7 @@ VALUES
 (1, '오리지날 콤보', '맛있으면 짖는 개', 4.5, '왈왈왈!! 왈왈!'),
 (2, '뿌링클', '전지현', 5, NULL);
 
-SELECT * FROM `review_review`;
+SELECT * FROM `new_review`;
 
 -- 3. 리뷰 출력
 -- 교촌치킨의 리뷰 리스트를 아래와 같은 형태로 조회하세요.
@@ -73,4 +78,5 @@ SELECT * FROM `review_review`;
 -- 오리지날 콤보	이병헌	4.5	콤보는 항상 진리입니다.
 -- 오리지날 콤보	맛있으면짖는개	4.5	왈왈왈!! 왈왈!
 
-SELECT `menu`, `userName`, `point`, `review` FROM `review_review` WHERE `menu` = '오리지날 콤보'; 
+SELECT * FROM `store` WHERE `name` = '교촌치킨';
+SELECT `menu`, `userName`, `point`, `review` FROM `new_review` WHERE `storeId` = 1; 
