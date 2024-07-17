bad5c54944bb34b5eee952071cf6452362e31fe5
garden <124008155+dengar7110@users.noreply.github.com>
2024-07-17 21:48:09 +0900

    Update Ex01Controller.java
    0717

--- Servlet/com/garden/servlet/servlet/ex/Ex01Controller.java
+++ Servlet/com/garden/servlet/servlet/ex/Ex01Controller.java
@@ -1,4 +1,4 @@
-package com.garden.servlet.servlet.test;
+package com.garden.servlet.servlet.ex;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -9,28 +9,38 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class Test01Controller extends HttpServlet {
-	
-	
+public class Ex01Controller extends HttpServlet {
+
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		
+		// response header
+		// 응답 데이터에 대한 정보
+		// character set : utf-8
 		response.setCharacterEncoding("utf-8");
+		// 데이터 타입
+		// MIME
+		// text/plain : 그냥 문자열
 		response.setContentType("text/plain");
-		PrintWriter out = response.getWriter();
 		
+		// response body
+		PrintWriter out = response.getWriter();		
+		out.println("URL Mapping 예제");
 		
-		
+		// 현재날짜 시간 response 에 담기
 		Date now = new Date();
 		
-		SimpleDateFormat formatter = new SimpleDateFormat("yyyy년 M월 dd일");
+		out.println(now);
 		
-		String dateString = formatter.format(now);
+		// 2024년 07월 17일 21:28:30
+		SimpleDateFormat fomatter = new SimpleDateFormat("yyyy년 MM월 dd일 HH:mm:ss");
 		
-		out.println("오늘의 날짜는 " + dateString);
-	
+		String dateTimeString = fomatter.format(now);
+		
+		out.println(dateTimeString);
+
 		
 		
 	}
-
+	
+	
 }
