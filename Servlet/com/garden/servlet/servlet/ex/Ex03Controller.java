dd20ac5b51567ff605ef864da7ffc525d2823166
garden <124008155+dengar7110@users.noreply.github.com>
2024-07-19 20:17:59 +0900

    Update Ex03Controller.java
    0719

--- Servlet/com/garden/servlet/servlet/ex/Ex03Controller.java
+++ Servlet/com/garden/servlet/servlet/ex/Ex03Controller.java
@@ -15,7 +15,8 @@ public class Ex03Controller extends HttpServlet {
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		
 		response.setCharacterEncoding("utf-8");
-		response.setContentType("text/html");
+//		response.setContentType("text/html");
+		response.setContentType("application/json");
 		PrintWriter out = response.getWriter();
 		
 		// 이름과 생년월일을 전달받고,
@@ -29,10 +30,18 @@ public class Ex03Controller extends HttpServlet {
 		
 		int age = 2024 - year + 1;
 		
-		out.println("<html><head><title>이름,나이</title></head><body>");
-		out.println("<h2> 이름 : " + name + "</h2>");
-		out.println("<h2> 나이 : " + age + "</h2>");
-		out.println("</body></html>");
+//		out.println("<html><head><title>이름,나이</title></head><body>");
+//		out.println("<h2> 이름 : " + name + "</h2>");
+//		out.println("<h2> 나이 : " + age + "</h2>");
+//		out.println("</body></html>");
+		
+		// 이름, 나이
+		// response 에 데이터만 담아서 보내야 하는 경우
+		// 데이터를 표현하는 규격을 통해 문자열을 담는다.
+		// json
+		// {"name":"홍정원","age":"30"}
+		out.println("{\"name\":\"" + name + "\", \"age\":" + age + "}");
+		
 		
 	}
 
